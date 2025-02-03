d277f05b5ada228070db2d1e5818f93da8c3ba1d
syaiba.pramudita <syaiba.pramudita@solusi247.com>
2025-02-04 01:53:25 +0700

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -36,48 +36,6 @@
       </div>
     </div>
   </div>
-
-  <!-- About Section -->
-  <div class="container my-5 py-5">
-      <div class="row mb-5">
-          <div class="col-12">
-              <h1 class="fw-bold text-center">Top List Vacation</h1>
-              <hr>
-          </div>
-      </div>
-      <div class="row">
-            <div class="col-md-4">
-                <div class="card p-1" style="width: 18rem;">
-                    <img src="Front-End.jpg" class="card-img-top" alt="fornt end">
-                    <div class="card-body">
-                      <h5 class="card-title text-center">Swagger Rest API Sistem Speech Recognition</h5>
-                      <p class="card-text">Membuat serta mengatur logic dari management data speech regocnition. Validasi dan mengubah data suara dari front end untuk disimpan kedalam database</p>
-                      <a href="#" class="btn btn-primary">Repository Saya</a>
-                    </div>
-                  </div>
-              </div>
-              <div class="col-md-4">
-                <div class="card p-1" style="width: 18rem;">
-                    <img src="backend-developer.jpg" class="card-img-top" alt="Back end">
-                    <div class="card-body">
-                      <h5 class="card-title text-center">Swagger Rest API management Store</h5>
-                      <p class="card-text">embuat serta mengatur logic dari management data store. Validasi dan mengubah data suara dari front end untuk disimpan kedalam database.</p>
-                      <a href="#" class="btn btn-primary">Repository Saya</a>
-                    </div>
-                  </div>
-              </div>
-              <div class="col-md-4">
-                <div class="card p-1" style="width: 18rem;">
-                    <img src="database.jpg" class="card-img-top" alt="Database">
-                    <div class="card-body">
-                      <h5 class="card-title text-center">Database Managemen</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Repository Saya</a>
-                    </div>
-                  </div>
-              </div>
-      </div>
-  </div>
 </template>
 
 <script>
